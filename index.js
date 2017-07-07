a4996f4dd22bfad60ff9e468d4671dbf541162e3
Jschles1 <jschles1@ramapo.edu>
2017-07-06 22:14:10 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function deepestChild() {
+  // return document.getElementById('grand-node').children[0].children[0].children[0].children[0]
+  var divs = document.getElementById('grand-node')
+  let child = divs.children[0]
+
+  while (child) {
+    divs = child
+    child = divs.children[0]
+  }
+
+  return divs
+}
+
+function increaseRankBy(n) {
+  const ranked = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < ranked.length; i++) {
+    var numbersLists = ranked[i].children
+
+    for (let ii = 0; ii < numbersLists.length; ii++) {
+      numbersLists[ii].innerHTML = parseInt(numbersLists[ii].innerHTML) + n
+    }
+  }
+
+  return ranked
+}
